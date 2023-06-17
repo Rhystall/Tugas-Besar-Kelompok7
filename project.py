1db501329fc2c567a15c0a676c1228925be52778
AubreyR.F <58387657+Aubrey16@users.noreply.github.com>
2023-06-17 09:19:03 +0700

    Update project.py
    Main function

--- project.py
+++ project.py
@@ -1,1 +1,34 @@
-#LALALALALALALALALALALALA WOOOOIIII
+Cash = 0
+Bank = 0
+
+MasukanUang = int(input("Masukan jumlah uang : "))
+print("1) Cash")
+print("2) Bank")
+MasukanKemana = int(input("Mau dimasukan kemana : "))
+
+if MasukanKemana == 1 :
+    Cash =+ MasukanUang
+elif MasukanKemana == 2 :
+    Bank =+ MasukanUang
+else :
+    print("salah")
+
+DecisionCounter = 0
+
+cuy = (1,2)
+
+while DecisionCounter == 0:
+    print("jumlah uang cash mu adalah :", Cash)
+    print("jumalah uang di bank mu adalah :", Bank)
+    # MauNgapain = input("Mau ngapain :")
+    Decision = int(input("ketik 1. untuk lanjut dan 2. untuk stop : "))
+    if Decision == 2 :
+        DecisionCounter =+ 1
+    elif Decision == 1 :
+        print("ok")
+    else :
+        while Decision not in cuy :
+            MasukanLagi = int(input("Salah ulang lagi :"))
+            Decision = MasukanLagi
+            if Decision == 2 :
+                DecisionCounter =+ 1
