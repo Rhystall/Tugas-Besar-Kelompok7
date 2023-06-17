ef6515075e6ce708ecc2f1bf081631db0e283945
AubreyR.F <aubrey.rayhan.firoz@gmail.com>
2023-06-17 19:08:58 +0700

    Function transfer

--- project.py
+++ project.py
@@ -1,40 +1,73 @@
+import sys
+
 Cash = 0
 Bank = 0
+rekening = [123, 412]
+cuy = (1, 2)
 
-MasukanUang = int(input("Masukan jumlah uang : "))
+
+def transfer(Bank):
+    Transfer = int(input("Berapa yang mau di transferkan: "))
+    Tujuan = int(input("Masukkan rekening yang mau di transfer: "))
+
+    while True:
+        if Tujuan not in rekening:
+            ulang = int(input("Rekening tidak ditemukan, coba lagi: "))
+            Tujuan = ulang
+        else:
+            Bank -= Transfer + 5000
+            if Bank < 0:
+                print("Transfer gagal, dana kurang.")
+                Bank += Transfer + 5000
+            else:
+                print("Transfer berhasil, Anda akan dikenakan biaya 5000.")
+            return Bank
+
+
+MasukanUang = int(input("Masukkan jumlah uang: "))
 print("1) Cash")
 print("2) Bank")
-MasukanKemana = int(input("Mau dimasukan kemana : "))
+MasukanKemana = int(input("Mau dimasukkan kemana: "))
 
-if MasukanKemana == 1 :
-    Cash =+ MasukanUang
-elif MasukanKemana == 2 :
-    Bank =+ MasukanUang
-else :
-    print("salah")
+if MasukanKemana == 1:
+    Cash += MasukanUang
+elif MasukanKemana == 2:
+    Bank += MasukanUang
+else:
+    print("Salah")
 
 DecisionCounter = 0
 
-cuy = (1,2)
-
 while DecisionCounter == 0:
-    print("jumlah uang cash mu adalah :", Cash)
-    print("jumalah uang di bank mu adalah :", Bank)
-    # MauNgapain = input("Mau ngapain :")
-    Decision = int(input("ketik 1. untuk lanjut dan 2. untuk stop : "))
-    if Decision == 2 :
-        DecisionCounter =+ 1
-    elif Decision == 1 :
-        print("ok")
-    else :
-        while Decision not in cuy :
-            MasukanLagi = int(input("Salah ulang lagi :"))
+    print("Jumlah uang cashmu adalah:", Cash)
+    print("Jumlah uang di bankmu adalah:", Bank)
+    print()
+    print("===== 0) ga ngapa ngapain 2 jam =====")
+    print("===== 1) transfer ===================")
+    print()
+    Ngapain = input("")
+    if Ngapain == "0":
+        print("heeee, ok")
+        sys.exit()
+    elif Ngapain == "1":
+        Bank = transfer(Bank)
+    
+    print()
+    print("1) lanjut")
+    print("2) Keluar")
+    Decision = int(input("Apa yang mau dipilih : "))
+    if Decision == 2:
+        DecisionCounter += 1
+    elif Decision == 1:
+        print("OK")
+    else:
+        while Decision not in cuy:
+            MasukanLagi = int(input("Salah, ulangi lagi: "))
             Decision = MasukanLagi
-            if Decision == 2 :
-                DecisionCounter =+ 1
-
+            if Decision == 2:
+                DecisionCounter += 1
 #OTW
-def tranfer()
-def pemasukan()
-def history()
-def pengeluaran()
+
+# def pemasukan():
+# def history():
+# def pengeluaran():
